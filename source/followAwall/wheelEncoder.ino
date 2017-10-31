f419500bdba1b572d51b224d52a3182decdd1f88
Donovan Keohane <admin@donovank.net>
2017-10-31 07:46:42 -0400

    save

--- source/followAwall/wheelEncoder.ino
+++
@@ -1,8 +0,0 @@
-//---------Interrup Sevice Routine for Wheel encoder------------------------
-void wheel()
-{
-  count++;                      //increment count by one whenever function is called.
- // Serial.print ("w,");          
-//  Serial.println (count);
-}
-
---
+++ src/regionals/followMe.ino
@@ -0,0 +1,65 @@
+
+//Iditarod Rover
+/*L         R    
+ --         --   [] --> rover veers to RIGHT with this program
+|  |__[ ]__|  |  [] --> ultra sonic sensor points this way 
+ --| front |--   []
+   |       |     []
+   |       |     []
+ --|       |--   [] 
+|  | back  |  |  []
+ --|_______|--   []
+   |_|           []
+   (_)           [] 
+ */
+int distance;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  distance = getDistance();       // Call getDistance function to get value in inches.. 
+  Serial.println(distance);  
+  if (distance > 10) {            // If object is over 10 inches away.
+    roveDrive(HIGH,HIGH,50,100);  // Keep driving!
+  } else {
+    roveStop();                   // Otherwise stop.
+  }
+}
+
+//----------------------FUNCTIONS -----------------------------------
+
+//-----------------------Get Distance--------------------
+int getDistance()
+{
+  const int sonarPin = 7; 
+  long pulse;                             // Declare long variable to read pulses from sensor.
+  int loops = 3;                          // Use to define the number of loops.
+  int tempInches;
+  int tempTotal=0;
+
+  for (int i=1; i <= loops; i++) {
+    pulse = pulseIn(sonarPin, HIGH);      //Read sensor
+    tempInches = pulse/147;
+    tempTotal = tempTotal + tempInches;   //Calculate inches and add to total
+  }
+
+  tempInches = tempTotal/loops;           // divide total by number of loops run
+  return tempInches;                      // return inches.
+}
+
+void roveDrive(bool MotorA, bool MotorB, int speedA, int speedB)
+{
+  digitalWrite(12,!MotorA);
+  digitalWrite(13,!MotorB);
+  analogWrite(3,speedA);
+  analogWrite(11,speedB); 
+}
+
+void roveStop()
+{
+  analogWrite(3,0);
+  analogWrite(11,0); 
+  delay(1);
+}
